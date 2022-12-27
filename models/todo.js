9b88cbb4aa859b13923d8ecdba2ca7598f1abac9
Yu-Che-Chang <a79967167@gmail.com>
2022-12-27 19:27:20 +0800

    feat: add mongoose

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose') //把 mongoose 載入並使用以下方法
+// 資料庫綱要
+const Schema = mongoose.Schema
+
+// new + Schema 物件建構子(關鍵字：物件導向 object mapping)
+const todoSchema = new Schema({
+  name: {
+    type: String,  // 資料型別事字串
+    required: true // 這個是必填欄位
+  },
+  done: {
+    type: Boolean
+  }
+})
+// 透過 module.exports 輸出
+// 把定義的 Schema 編譯成可供操作的model物件,並取名成Todo
+module.exports = mongoose.model('Todo', todoSchema)
